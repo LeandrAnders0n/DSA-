756c74ad841e16228737f76eb6848ff9eb221494
LeandraAnderson <110786226+LeandraAnderson@users.noreply.github.com>
2023-09-09 18:55:25 +0530

    Tree: Construct Tree from Postorder and Inorder

---
+++ Tree/tree-from-postorder-inorder-traversal.py
@@ -0,0 +1,39 @@
+#Question: Construct Binary Tree from Preorder and Inorder Traversal
+#Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+#Approach:
+# Construct a binary tree from its inorder and postorder traversals using recursion. Extract the root value from the end of the postorder list, find its position in the inorder list to divide it into left and right subtrees, and then recursively build these subtrees. 
+
+# Time Complexity & Space Complexity: O(n)
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def construct_tree(inorder, postorder):
+    if not inorder or not postorder:
+        return None
+    root_val = postorder.pop()
+    root = TreeNode(root_val)
+    root_index = inorder.index(root_val)
+
+    root.right = construct_tree(inorder[root_index + 1:], postorder)
+    root.left = construct_tree(inorder[:root_index], postorder)
+    
+    return root
+
+
+
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+print(construct_tree(inorder,postorder))
+# Output: [3,9,20,null,null,15,7]
+
+inorder = [-1]
+postorder = [-1]
+# Output: [-1]
+print(construct_tree(inorder,postorder))
