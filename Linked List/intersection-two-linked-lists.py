1a08b6e60c838d1119e2eb66af6174dfc0906774
LeandraAnderson <110786226+LeandraAnderson@users.noreply.github.com>
2023-12-16 19:44:28 +0530

    Linked Lists: 160. Intersection of Two Linked Lists

---
+++ Linked List/intersection-two-linked-lists.py
@@ -0,0 +1,41 @@
+#Question: 160. Intersection of Two Linked Lists
+# Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+# For example, the following two linked lists begin to intersect at node c1:
+# The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+# Note that the linked lists must retain their original structure after the function returns.
+
+# Custom Judge:
+# The inputs to the judge are given as follows (your program is not given these inputs):
+# intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+# listA - The first linked list.
+# listB - The second linked list.
+# skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+# skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+# The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+#Approach: Two Pointers
+# Find the intersection point of two linked lists by using two pointers, iterating through the lists simultaneously and adjusting pointers when reaching the end. Return either the intersection node or None if there is no intersection. 
+
+# Time complexity: O(m + n), where m and n are the lengths of the linked lists
+# Space complexity: O(1).
+
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None 
+
+        ptrA, ptrB = headA, headB
+
+        while ptrA != ptrB:
+            ptrA = ptrA.next if ptrA else headB
+            ptrB = ptrB.next if ptrB else headA
+
+        return ptrA  
+
