a2ae97a7844b781695e6ca911d01c905b4c473a3
LeandraAnderson <110786226+LeandraAnderson@users.noreply.github.com>
2024-03-28 23:01:26 +0530

    Arrrays: 2958. Length of Longest Subarray With at Most K Frequency

--- Array/length-longest-subarray-with-k-frequency.py
+++ Array/length-longest-subarray-with-k-frequency.py
@@ -1,4 +1,4 @@
-#Quetion: 2958. Length of Longest Subarray With at Most K Frequency
+#Question: 2958. Length of Longest Subarray With at Most K Frequency
 # You are given an integer array nums and an integer k.
 # The frequency of an element x is the number of times it occurs in an array.
 # An array is called good if the frequency of each element in this array is less than or equal to k.
